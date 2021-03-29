a7c17536d59f3443baa5fb7bcfee111e31293b9a
mitsuru_yamaura <komayou326@gmail.com>
2021-03-29 17:26:01 +0900

    お使いを開始するとタップポイントの画像を切り替える制御を追加。CharaController.cs 作成。 お使いが完了すると、完了したタップポイントの位置にキャラを生成。各スクリプトの修正。デバッグ完了。

--- Assets/Scripts/TapPointDetail.cs
+++ Assets/Scripts/TapPointDetail.cs
@@ -1,13 +1,21 @@
+using DG.Tweening;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using DG.Tweening;
 
 public class TapPointDetail : MonoBehaviour
 {
     [SerializeField]
     private Button btnTapPoint;
+
+    [SerializeField]
+    private Image imgTapPoint;
+
+    [SerializeField]
+    private Sprite jobSprite;
+
+    [SerializeField]
+    private Sprite defaultSprite;
 
     [SerializeField, Header("このタップポイントのジョブ番号")]
     private int myJobNo;
@@ -15,6 +23,10 @@ public class TapPointDetail : MonoBehaviour
     public JobData jobData;
 
     private GameManager gameManager;
+
+    public bool isJobs;
+
+    Tween tween;
 
     void Start() {
         btnTapPoint.onClick.AddListener(OnClickTapPoint);
@@ -46,6 +58,48 @@ public class TapPointDetail : MonoBehaviour
     /// </summary>
     /// <param name="isSwitch"></param>
     public void SwtichActivateTapPoint(bool isSwitch) {
-        btnTapPoint.gameObject.SetActive(isSwitch);
+        imgTapPoint.enabled = isSwitch;
+        btnTapPoint.enabled = isSwitch;
+
+        transform.localScale = Vector3.one;
+    }
+
+    /// <summary>
+    /// 仕事中の画像に変更
+    /// </summary>
+    public void ChangeJobSprite() {
+        imgTapPoint.sprite = jobSprite;
+        transform.localScale = new Vector3(1.75f, 1.0f, 1.0f);
+
+        tween = transform.DOPunchPosition(new Vector3(10.0f, 0, 0), 3.0f, 10, 3)
+            .SetEase(Ease.Linear)
+            .OnComplete(() => { })            
+            .SetLoops(-1, LoopType.Restart);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator StartJobs() {
+
+        while (isJobs) {
+            // TODO 条件として時間を確認する
+
+            yield return new WaitForSeconds(3.0f);
+
+            isJobs = false;
+
+            tween.Kill();
+
+            yield return null;
+        }
+
+        // 仕事終了
+        Debug.Log("お使い 終了");
+
+        // TODO ゲームマネージャーでキャラ生成
+        gameManager.GenerateChara(this);
+
     }
 }
