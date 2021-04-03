b0bb4162f77848ef131c38e984897dd4501ba7a5
mitsuru_yamaura <komayou326@gmail.com>
2021-04-04 08:46:44 +0900

    GameData.cs と、入れ子クラスとして EranedReward クラス作成。獲得した褒賞を EranedReward クラスの List として保持。褒賞ポイントを保持。 PlayerPrefsJsonUtility.cs と GameManager.cs を修正して、褒賞の獲得処理、セーブ処理、ゲーム起動時の褒賞のロード処理を追加。デバッグ完了。実機テスト完了。

---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,132 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData : MonoBehaviour
+{
+    public static GameData instance;
+
+    /// <summary>
+    /// 獲得済の褒賞の登録用クラス
+    /// </summary>
+    [System.Serializable]
+    public class EarnedReward {
+        public int rewardNo;      // 褒賞の番号 RewardData の rewardNo と照合する
+        public int rewardCount;   // 褒賞の所持数
+    }
+
+    [Header("獲得している褒賞リスト")]
+    public List<EarnedReward> earnedRewardsList = new List<EarnedReward>();
+
+    [Header("褒賞ポイント")]
+    public int totalRewardPoint;
+
+    private const string EARNED_REWARD_SAVE_KEY = "earnedRewardNo_";
+    private const string TOTAL_REWARD_POINT_SAVE_KEY = "totalRewardPoint";
+
+    private int maxRewardDataCount;
+
+    private void Awake() {
+        if(instance == null) {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+
+    /// <summary>
+    /// 獲得した褒賞をリストに追加
+    /// すでにリストにある場合には加算
+    /// </summary>
+    /// <param name="addRewardNo"></param>
+    /// <param name="addRewardCount"></param>
+    public void AddEarnedRewardsList(int addRewardNo, int addRewardCount = 1) {
+        // すでにリストに登録がある褒賞か確認する
+        if (earnedRewardsList.Exists(x => x.rewardNo == addRewardNo)) {
+            // 登録がある場合には加算
+            earnedRewardsList.Find(x => x.rewardNo == addRewardNo).rewardCount++;
+        } else {
+            // 登録がない場合には新しく追加
+            earnedRewardsList.Add(new EarnedReward { rewardNo = addRewardNo, rewardCount = addRewardCount });
+        }
+    }
+
+    /// <summary>
+    /// 褒賞ポイント計算
+    /// </summary>
+    /// <param name="amount"></param>
+    public void CalulateTotalRewardPoint(int amount) {
+        // 褒賞ポイントを計算して合計値算出
+        totalRewardPoint += amount;
+    }
+
+    /// <summary>
+    /// 獲得した褒賞のデータのセーブ
+    /// </summary>
+    /// <param name="saveRewardNo"></param>
+    public void SaveEarnedReward(int saveRewardNo) {
+        PlayerPrefsJsonUtility.SaveSetObjectData(earnedRewardsList.Find(x => x.rewardNo == saveRewardNo), EARNED_REWARD_SAVE_KEY + saveRewardNo);
+    }
+
+    /// <summary>
+    /// 獲得した褒賞のデータのロード
+    /// </summary>
+    public void LoadEarnedRewardData() {
+        for (int i = 0; i < maxRewardDataCount; i++) {
+            // セーブされている褒賞のデータが存在しているか確認
+            if (PlayerPrefsJsonUtility.ExistsData(EARNED_REWARD_SAVE_KEY + i)) {
+                // セーブデータがある場合のみロード
+                EarnedReward earnedReward = PlayerPrefsJsonUtility.LoadGetObjectData<EarnedReward>(EARNED_REWARD_SAVE_KEY + i);
+                // リストに追加
+                AddEarnedRewardsList(earnedReward.rewardNo, earnedReward.rewardCount);
+            }
+        }            
+    }
+
+    /// <summary>
+    /// 獲得している褒賞のリストを削除
+    /// 番号指定ありの場合には指定された褒賞の番号の情報を削除
+    /// 番号指定なしの場合にはすべて削除
+    /// </summary>
+    /// <param name="isAll"></param>
+    public void RemoveEarnedRewardsList(int rewardNo = 999) {
+        if (rewardNo == 999) {
+            // すべての褒賞のデータを削除
+            earnedRewardsList.Clear();
+        } else {
+            // 指定された褒賞の番号のデータを削除
+            earnedRewardsList.Remove(earnedRewardsList.Find(x => x.rewardNo == rewardNo));
+        }
+    }
+
+    /// <summary>
+    /// 褒賞データの最大数を登録
+    /// </summary>
+    /// <param name="maxCount"></param>
+    public void GetMaxRewardDataCount(int maxCount) {
+        maxRewardDataCount = maxCount;
+    }
+
+    /// <summary>
+    /// 獲得している褒賞データの最大数の取得
+    /// </summary>
+    /// <returns></returns>
+    public int GetEarnedRewardsListCount() {
+        return earnedRewardsList.Count;
+    }
+
+    /// <summary>
+    /// 褒賞ポイントのセーブ
+    /// </summary>
+    public void SaveTotalRewardPoint() {
+        PlayerPrefsJsonUtility.SaveIntData(TOTAL_REWARD_POINT_SAVE_KEY, totalRewardPoint);
+    }
+
+    /// <summary>
+    /// 褒賞ポイントのロード
+    /// </summary>
+    public void LoadTotalRewardPoint() {
+        totalRewardPoint = PlayerPrefsJsonUtility.LoadIntData(TOTAL_REWARD_POINT_SAVE_KEY);
+    }
+}
